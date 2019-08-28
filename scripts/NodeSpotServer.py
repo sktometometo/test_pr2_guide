dc4fafa0a6bed6fdebfa55497a583d752c9aa6a6
Koki Shinjo <sktometometo@gmail.com>
2019-08-28 17:25:35 +0900

    fix bugs in NodeSpotServer.py to run

--- scripts/NodeSpotServer.py
+++ scripts/NodeSpotServer.py
@@ -16,31 +16,28 @@ class SpotServer:
         self.nodename = "spot_manager"
         rospy.init_node( self.nodename )
         ##
-        self.servicename_add    = rospy.getparam( "/pr2_guide/servicename/spot_manager/add",    "/spot_manager/add" )
-        self.servicename_delete = rospy.getparam( "/pr2_guide/servicename/spot_manager/delete", "/spot_manager/delete" )
-        self.servicename_get    = rospy.getparam( "/pr2_guide/servicename/spot_manager/get",    "/spot_manager/get" )
-        self.servicename_save   = rospy.getparam( "/pr2_guide/servicename/spot_manager/save",   "/spot_manager/save" ) 
+        self.servicename_add    = rospy.get_param( "/pr2_guide/servicename/spot_manager/add",    "/spot_manager/add" )
+        self.servicename_delete = rospy.get_param( "/pr2_guide/servicename/spot_manager/delete", "/spot_manager/delete" )
+        self.servicename_get    = rospy.get_param( "/pr2_guide/servicename/spot_manager/get",    "/spot_manager/get" )
+        self.servicename_save   = rospy.get_param( "/pr2_guide/servicename/spot_manager/save",   "/spot_manager/save" ) 
         ##
         self.service_add    = rospy.Service( self.servicename_add,    SpotManagerAdd,    self.servicehandler_add )
-        self.service_delete = rospy.Service( self.servicename_detele, SpotManagerDelete, self.servicehandler_delete )
+        self.service_delete = rospy.Service( self.servicename_delete, SpotManagerDelete, self.servicehandler_delete )
         self.service_get    = rospy.Service( self.servicename_get,    SpotManagerGet,    self.servicehandler_get )
         self.service_save   = rospy.Service( self.servicename_save,   SpotManagerSave,   self.servicehandler_save )
         # SpotManager
-        self.spotlist = []
+        self.spotlist = {}
         #
         try:
-            self.filepath = rospy.getparam( "/pr2_guide/filename/spot_manager/config" )
-            if os.path.exits( self.filepath ):
+            self.filepath = rospy.get_param( "/pr2_guide/filename/spot_manager/config" )
+            if os.path.exists( self.filepath ):
                 self.load_spotlist( self.filepath )
+            print( "initialize finished." )
         except KeyError:
             self.filepath = None
 
     def __del__( self ):
         self.save_spotlist( self.filepath )
-
-    def update_id( self ):
-        for index, spot in self.spotlist:
-            spot.id = index
 
     def load_spotlist( self, filepath ):
         if self.filepath is None:
@@ -53,7 +50,7 @@ class SpotServer:
             return False
 
     def save_spotlist( self, filepath ):
-        if self.path is None:
+        if self.filepath is None:
             return False
         else:
             with open( filepath, "wb" ) as f:
@@ -61,21 +58,27 @@ class SpotServer:
             return True
 
     def servicehandler_add( self, req ):
-        self.spotlist.append( req.spot )
-        self.update_id()
+        print( "add service called" )
+        spot = req.spot
+        if spot.name in self.spotlist.keys():
+            return SpotManagerAddResponse( False )
+        self.spotlist[ spot.name ] = spot
         return SpotManagerAddResponse( True )
 
     def servicehandler_delete( self, req ):
-        del self.spotlist[ req.id ]
-        self.update_id()
+        print( "del service called" )
+        if req.name in self.spotlist.keys():
+            self.spotlist.pop( req.name )
         return SpotManagerDeleteResponse( True )
 
     def servicehandler_get( self, req ):
-        return SpotManagerGetResponse( self.spotlist )
+        print( "get service called" )
+        return SpotManagerGetResponse( list( self.spotlist.values() ) )
 
     def servicehandler_save( self, req ):
-        self.save_spotlist( self.filepath )
-        return SpotManagerSaveResponse( True )
+        print( "save service called" )
+        ret = self.save_spotlist( self.filepath )
+        return SpotManagerSaveResponse( ret )
 
 
 if __name__ == "__main__":
